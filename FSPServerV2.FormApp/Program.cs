cfcad5ffc35eb360cc3a97a642efabcffbd6508b
Mobile Aviation Tools <rob@robenanita.nl>
2018-03-06 18:11:34 +0100

    windows notify icon form app added

---
+++ FSPServerV2.FormApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FSPServerV2.FormApp
+{
+    static class Program
+    {
+        static Mutex mutex = new Mutex(false, "Flight Sim Planner V2 Server Mutex");
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(2), false))
+            {
+                return;
+            }
+            else
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
